b09e640231f80cb2e55a738126c38f25f72a8423
Timothy <timmy.mortensen@hotmail.com>
2025-03-06 14:30:34 +0100

    .

--- triggers/AfterInsertMatch/testAfterInsertMatch.sql
+++ triggers/AfterInsertMatch/testAfterInsertMatch.sql
@@ -1,2 +1,2 @@
-INSERT INTO Matches (tournament_id, player1_id, player2_id, winner_id)
-VALUES (7, 1, 2, 1);
+INSERT INTO Matches (tournament_id, player1_id, player2_id, winner_id, match_date)
+VALUES (7, 1, 2, 1, NOW());
